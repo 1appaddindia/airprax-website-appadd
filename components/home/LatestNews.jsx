6abc7c7180e2bb372d5670fd084a8056eeded246
appadd <indiaappadd@gmail.com>
2024-06-11 14:06:14 +0530

    feat/navbar

--- components/home/LatestNews.jsx
+++ components/home/LatestNews.jsx
@@ -7,7 +7,7 @@ function LatestNews({ className, ...props }) {
   const latestNewsSlice = latestNews.slice(0, 4);
   return (
     <div className="bg-[#e5ebf1] pt-1 lg:mt-0 lg:p-5">
-      <div className="lg:p-5 lg:container lg:max-w-9xl lg:mt-60 mt-56">
+      <div className="lg:p-5 lg:container lg:max-w-9xl lg:mt-60 mt-64">
         <div className=" text-center lg:mt-10 m-8">
           <h6>Latest News</h6>
           <h1 className=" font-bold lg:text-4xl text-lg">
@@ -19,12 +19,16 @@ function LatestNews({ className, ...props }) {
             <LatestNewsCard item={item} />
           ))}
         </div>
-          <Link href="/latest-news" className="flex items-center justify-center">
-            <button className="bg-[#bf1e2e] m-5 lg:w-36 text-white lg:text-lg text-sm rounded font-bold p-2 flex items-center gap-2">
-              Read More
-            <img src="/images/flaticons/fast-forward.png" alt="" className="h-5 w-5"/>
-            </button>
-          </Link>
+        <Link href="/latest-news" className="flex items-center justify-center">
+          <button className="bg-[#bf1e2e] m-5 lg:w-36 text-white lg:text-lg text-sm rounded font-bold p-2 flex items-center gap-2">
+            Read More
+            <img
+              src="/images/flaticons/fast-forward.png"
+              alt=""
+              className="h-5 w-5"
+            />
+          </button>
+        </Link>
       </div>
     </div>
   );
