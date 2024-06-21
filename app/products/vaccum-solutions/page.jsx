aee63f64464cee741abc7b03314c01001df27e75
appadd <indiaappadd@gmail.com>
2024-06-21 10:45:11 +0530

    feat/products

--- app/products/vaccum-solutions/page.jsx
+++ app/products/vaccum-solutions/page.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Banner from "../../../components/products/vaccum-products/Banner";
 import Products from "../../../components/products/vaccum-products/Products";
 
 const VaccumSolutions = () => {
   return (
     <div>
-      <Banner />
       <Products />
     </div>
   );
