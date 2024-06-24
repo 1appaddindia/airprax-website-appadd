00c19972b95b2e0ddd1c44fa287f15e9ac30be11
appadd <indiaappadd@gmail.com>
2024-06-24 11:36:39 +0530

    feat/brochures

--- utils/brochure/data.js
+++ utils/brochure/data.js
@@ -1,27 +1,26 @@
 export const pdfFiles = [
-    {
-        name: "Brochure 1",
-        path: " G2-5 G7-15 G15L-22_PNE_Leaflet.pdf"
-    },
-    {
-        name: "Brochure 2",
-        path: " GA 7-22 VSD iPM Sales Lealfet EN Pune.pdf"
-    },
-    {
-        name: "Brochure 3",
-        path: " GA 15-30 2021 Sales Leaflet EN Pune (1).pdf"
-    },
-    {
-        name: "Brochure 4",
-        path: " GA 30-90 (VSD) Sales Leaflet EN Pune 9096 3256 30 (1).pdf"
-    },
-    {
-        name: "Brochure 5",
-        path: " GA 30+-90 (2022) AC 50Hz Sales Leaflet EN Pune.pdf"
-    },
-    {
-        name: "Brochure 6",
-        path: " Leaflet GA 7-37 VSD+ English 2935 0981 45.pdf"
-    },
-
-]
+  {
+    name: "Brochure 1",
+    path: " G2-5 G7-15 G15L-22_PNE_Leaflet.pdf",
+  },
+  {
+    name: "Brochure 2",
+    path: " GA 7-22 VSD iPM Sales Lealfet EN Pune.pdf",
+  },
+  {
+    name: "Brochure 3",
+    path: " GA 15-30 2021 Sales Leaflet EN Pune (1).pdf",
+  },
+  {
+    name: "Brochure 4",
+    path: " GA 30-90 (VSD) Sales Leaflet EN Pune 9096 3256 30.pdf",
+  },
+  {
+    name: "Brochure 5",
+    path: " GA 30+-90 (2022) AC 50Hz Sales Leaflet EN Pune.pdf",
+  },
+  {
+    name: "Brochure 6",
+    path: " Leaflet GA 7-37 VSD+ English 2935 0981 45.pdf",
+  },
+];
