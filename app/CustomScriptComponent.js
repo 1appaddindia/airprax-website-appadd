4cf9e55f98f8f23776c698b80ae3cf695af9c1dc
appadd <indiaappadd@gmail.com>
2024-07-15 17:40:46 +0530

    feat/brochures

--- app/CustomScriptComponent.js
+++ app/CustomScriptComponent.js
@@ -1,42 +1,59 @@
 "use client";
+
 import { useEffect } from "react";
 
 const CustomScriptComponent = () => {
   useEffect(() => {
-    // Check if jQuery is loaded, otherwise load it dynamically
-    if (!window.jQuery) {
-      const script = document.createElement("script");
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
-      script.async = true;
-      document.body.appendChild(script);
-    }
-
-    // Execute your custom script
-    const eppathurl = window.location.origin + window.location.pathname;
-    const eptagmanage = new XMLHttpRequest();
-    eptagmanage.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        if (this.response !== 0) {
-          const temp = this.response.split("||||||||||");
-          document.head.querySelector("title").remove(); // Remove existing title
-          document.head.insertAdjacentHTML("beforeend", temp[0]); // Add new title content to head
-          document.body.insertAdjacentHTML("beforeend", temp[1]); // Append body content
-        }
+    // Function to load jQuery dynamically if not already loaded
+    const loadjQuery = () => {
+      if (!window.jQuery) {
+        const script = document.createElement("script");
+        script.src =
+          "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+        script.async = true;
+        document.body.appendChild(script);
+        return script; // Return the script element for cleanup
       }
+      return null;
     };
-    eptagmanage.open(
-      "GET",
-      atob(
-        "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
-      ) + eppathurl
-    );
-    eptagmanage.send();
+
+    // Execute your custom script after jQuery is loaded
+    const executeCustomScript = () => {
+      const eppathurl = window.location.origin + window.location.pathname;
+      const eptagmanage = new XMLHttpRequest();
+      eptagmanage.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          if (this.response !== 0) {
+            const temp = this.response.split("||||||||||");
+            document.head.querySelector("title").remove(); // Remove existing title
+            document.head.insertAdjacentHTML("beforeend", temp[0]); // Add new title content to head
+            document.body.insertAdjacentHTML("beforeend", temp[1]); // Append body content
+          }
+        }
+      };
+      eptagmanage.open(
+        "GET",
+        atob(
+          "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
+        ) + eppathurl
+      );
+      eptagmanage.send();
+    };
+
+    // Load jQuery if not already loaded
+    const script = loadjQuery();
+
+    // Execute custom script after jQuery is loaded
+    if (script) {
+      script.onload = executeCustomScript;
+    } else {
+      executeCustomScript();
+    }
 
     // Clean up on unmount
     return () => {
       // Remove jQuery if it was dynamically added
-      if (!window.jQuery) {
+      if (script && !window.jQuery) {
         document.body.removeChild(script);
       }
     };
