a20b1027e8f0007e8e3133af7a453cd125d44574
appadd <indiaappadd@gmail.com>
2024-07-15 18:05:48 +0530

    feat/brochures

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/global/Navbar";
 import Footer from "../components/global/Footer";
+import CustomScriptComponent from "./CustomScriptComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,57 +20,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [loader, setLoader] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoader(false);
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    // Load jQuery if not already loaded
-    if (typeof window !== "undefined" && !window.jQuery) {
-      const script = document.createElement("script");
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
-      document.head.appendChild(script);
-      script.onload = () => {
-        handleTagManage();
-      };
-    } else {
-      handleTagManage();
-    }
-  }, []);
-
-  const handleTagManage = () => {
-    var eppathurl = window.location.origin + window.location.pathname;
-    var eptagmanage = new XMLHttpRequest();
-    eptagmanage.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        if (this.response !== 0) {
-          var temp = this.response.split("||||||||||");
-
-          // Remove existing title from head and append the new one
-          $("head").find("title").remove();
-          $("head").append(temp[0]);
-          $("body").append(temp[1]);
-        }
-      }
-    };
-    eptagmanage.open(
-      "GET",
-      atob(
-        "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDQ4Njc4NjA0MDEmZWtleXBhc3M9SnV4NDJlaFdOUjJHZHFDMGxFdkhJcUFkOERMemlYdThUQnVIJnNpdGV1cmw9"
-      ) + eppathurl
-    );
-    eptagmanage.send();
-  };
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
         <main className="lg:mt-[128px] mt-[60px]">{children}</main>
         <Footer />
+        <CustomScriptComponent />
       </body>
     </html>
   );
