cd3b240b76645bca71e171feca8d1871ff98f7bf
appadd <indiaappadd@gmail.com>
2024-07-15 17:31:28 +0530

    feat/brochures

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/global/Navbar";
 import Footer from "../components/global/Footer";
+import CustomScriptComponent from "./CustomScriptComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main className="lg:mt-[128px] mt-[60px]">{children}</main>
         <Footer />
+        <CustomScriptComponent />
       </body>
     </html>
   );
