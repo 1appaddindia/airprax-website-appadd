9e58ef2da3d644072a983b4d8c8d462b3600444c
appadd <indiaappadd@gmail.com>
2024-07-17 17:18:46 +0530

    edit:footer

--- app/page.js
+++ app/page.js
@@ -1,14 +1,30 @@
-"use client";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom"; // Assuming you are using React Router
 import HomePage from "../app/home/page";
-
 import TagManager from "./TagManager";
 
-export default function Home() {
+const Home = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleUrlChange = () => {
+      window.location.reload();
+    };
+
+    // Listen for changes in the URL
+    const unlisten = history.listen(handleUrlChange);
+
+    return () => {
+      unlisten(); // Clean up the listener when component unmounts
+    };
+  }, [history]);
+
   return (
     <main className="overflow-hidden">
       <HomePage />
-
       <TagManager />
     </main>
   );
-}
+};
+
+export default Home;
