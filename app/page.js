32bd822b08d8b309146bf3731917f6e8c73c57d8
appadd <indiaappadd@gmail.com>
2024-07-17 17:19:48 +0530

    edit:footer

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom"; // Assuming you are using React Router
 import HomePage from "../app/home/page";
