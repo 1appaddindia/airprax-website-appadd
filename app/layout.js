5abe57be4423eb25895ed320eba4d32db47de3fd
appadd <indiaappadd@gmail.com>
2024-07-17 20:28:37 +0530

    edit:footer

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "../components/global/Navbar";
 import Footer from "../components/global/Footer";
 import Script from "next/script";
+import Plugin from "./Plugin";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,31 +27,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main className="lg:mt-[128px] mt-[60px]">{children}</main>
         <Footer />
-        <Script
-          src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-          strategy="beforeInteractive"
-        />
-        <Script id="custom-script" strategy="afterInteractive">
-          {`
-            window.jQuery || document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\\/script>");
-            var eppathurl = window.location.origin + window.location.pathname;
-            var eptagmanage = new XMLHttpRequest();
-            eptagmanage.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    if (this.response !== 0) {
-                        var temp = new Array();
-                        var mystr = this.response;
-                        temp = mystr.split("||||||||||");
-                        jQuery("head").find("title").remove();
-                        jQuery("head").append(temp[0]);
-                        jQuery("body").append(temp[1]);
-                    }
-                }
-            };
-            eptagmanage.open("GET", atob("aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9") + eppathurl);
-            eptagmanage.send();
-          `}
-        </Script>
+        <Plugin />
       </body>
     </html>
   );
