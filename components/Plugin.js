80724ad70c29ae656162c8d89911d659f5c11039
appadd <indiaappadd@gmail.com>
2024-07-17 23:19:39 +0530

    edit:footer

--- components/Plugin.js
+++ components/Plugin.js
@@ -8,35 +8,32 @@ function Plugin() {
       script.src =
         "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
       script.async = true;
-      document.head.appendChild(script);
-
       script.onload = () => {
-        const eppathurl = window.location.origin + window.location.pathname;
-        const eptagmanage = new XMLHttpRequest();
-        eptagmanage.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            if (this.response !== 0) {
-              const temp = this.response.split("||||||||||");
-              document.title = "";
-              // document.head.insertAdjacentHTML("beforeend", temp[0]);
-              // document.body.insertAdjacentHTML("beforeend", temp[1]);
-
-              if (typeof jQuery !== "undefined") {
-                jQuery("head")?.find("title")?.remove();
-                jQuery("head")?.append(temp[0]);
-                jQuery("body")?.append(temp[1]);
+        if (typeof jQuery !== "undefined") {
+          const eppathurl = window.location.origin + window.location.pathname;
+          const eptagmanage = new XMLHttpRequest();
+          eptagmanage.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+              if (this.response !== "0") {
+                const temp = this.response.split("||||||||||");
+                jQuery(document).ready(() => {
+                  jQuery("head").find("title").remove();
+                  jQuery("head").append(temp[0]);
+                  jQuery("body").append(temp[1]);
+                });
               }
             }
-          }
-        };
-        eptagmanage.open(
-          "GET",
-          atob(
-            "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
-          ) + eppathurl
-        );
-        eptagmanage.send();
+          };
+          eptagmanage.open(
+            "GET",
+            atob(
+              "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
+            ) + eppathurl
+          );
+          eptagmanage.send();
+        }
       };
+      document.head.appendChild(script);
     };
 
     loadScript();
