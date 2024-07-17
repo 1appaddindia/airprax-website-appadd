1ae8274a089bbee0143e0421b761ec38ea6d317b
appadd <indiaappadd@gmail.com>
2024-07-17 17:23:18 +0530

    edit:footer

--- app/page.js
+++ app/page.js
@@ -5,21 +5,6 @@ import HomePage from "../app/home/page";
 import TagManager from "./TagManager";
 
 const Home = () => {
-  const history = useHistory();
-
-  useEffect(() => {
-    const handleUrlChange = () => {
-      window.location.reload();
-    };
-
-    // Listen for changes in the URL
-    const unlisten = history.listen(handleUrlChange);
-
-    return () => {
-      unlisten(); // Clean up the listener when component unmounts
-    };
-  }, [history]);
-
   return (
     <main className="overflow-hidden">
       <HomePage />
