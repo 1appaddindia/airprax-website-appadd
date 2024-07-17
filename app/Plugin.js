519dd9bbaf9bd32f5dacab7a5a5850f050c28a81
appadd <indiaappadd@gmail.com>
2024-07-17 20:31:15 +0530

    edit:footer

--- app/Plugin.js
+++ app/Plugin.js
@@ -8,10 +8,10 @@ function Plugin() {
       <Script>
         {`
          window.jQuery ||
-document.write("<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\/script>");
+document.write("<script strategy="beforeInteractive" src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'><\/script>");
         `}
       </Script>
-      <Script>
+      <Script id="custom-script" strategy="afterInteractive">
         {`
             var eppathurl = window.location.origin + window.location.pathname;
     var eptagmanage = new XMLHttpRequest();
