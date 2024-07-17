46fae9274b4c9c29e02ede9f3bd8dc8d34b9e467
appadd <indiaappadd@gmail.com>
2024-07-17 23:48:47 +0530

    edit:footer

--- components/Plugin.js
+++ components/Plugin.js
@@ -1,63 +1,55 @@
 "use client";
-import { useEffect } from "react";
+
+import React, { useEffect } from "react";
 
 function Plugin() {
   useEffect(() => {
-    const loadScript = (src) => {
-      return new Promise((resolve, reject) => {
+    const loadCustomScript = () => {
+      // Check if jQuery is available
+      if (window.jQuery) {
+        // jQuery is already loaded, proceed with custom script
+        executeCustomScript();
+      } else {
+        // jQuery is not loaded, load it dynamically
         const script = document.createElement("script");
-        script.src = src;
-        script.async = true;
-        script.onload = resolve;
-        script.onerror = reject;
+        script.src =
+          "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+        script.onload = () => {
+          executeCustomScript();
+        };
         document.head.appendChild(script);
-      });
-    };
-
-    const loadAndExecute = async () => {
-      try {
-        // Load jQuery
-        await loadScript(
-          "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-        );
-
-        // Ensure jQuery is available
-        if (typeof jQuery !== "undefined") {
-          // Ensure DOM is ready using vanilla JavaScript
-          document.addEventListener("DOMContentLoaded", () => {
-            const eppathurl = window.location.origin + window.location.pathname;
-            const eptagmanage = new XMLHttpRequest();
-            eptagmanage.onreadystatechange = function () {
-              if (this.readyState === 4 && this.status === 200) {
-                if (this.response !== "0") {
-                  const temp = this.response.split("||||||||||");
-                  // Remove current title and append new content
-                  document.querySelector("title")?.remove();
-                  document.head.insertAdjacentHTML("beforeend", temp[0]);
-                  document.body.insertAdjacentHTML("beforeend", temp[1]);
-                }
-              }
-            };
-            eptagmanage.open(
-              "GET",
-              atob(
-                "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
-              ) + eppathurl
-            );
-            eptagmanage.send();
-          });
-        } else {
-          console.error("jQuery is not loaded");
-        }
-      } catch (error) {
-        console.error("Error loading jQuery or executing script", error);
       }
     };
 
-    loadAndExecute();
+    const executeCustomScript = () => {
+      // Execute your custom script here
+      var eppathurl = window.location.origin + window.location.pathname;
+      var eptagmanage = new XMLHttpRequest();
+      eptagmanage.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          if (this.response !== 0) {
+            var temp = new Array();
+            var mystr = this.response;
+            temp = mystr.split("||||||||||");
+            jQuery("head").find("title").remove();
+            jQuery("head").append(temp[0]);
+            jQuery("body").append(temp[1]);
+          }
+        }
+      };
+      eptagmanage.open(
+        "GET",
+        atob(
+          "aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDY2MzM3Nzc0MjUmZWtleXBhc3M9NGFoMXVtWEdlSjZTeVhFNkpmcG9VQTNsdWhFdGM0cmczQWZFJnNpdGV1cmw9"
+        ) + eppathurl
+      );
+      eptagmanage.send();
+    };
+
+    loadCustomScript();
   }, []);
 
-  return null;
+  return null; // Since this component doesn't render anything
 }
 
 export default Plugin;
