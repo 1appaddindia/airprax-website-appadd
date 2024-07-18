8b98a5f946418b3147553289f503722eab5bd4c2
appadd <indiaappadd@gmail.com>
2024-07-18 10:55:38 +0530

    edit:footer

--- components/latest-news/side-bar/Enquiry.jsx
+++ components/latest-news/side-bar/Enquiry.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import queryString from "query-string";
 
 const Enquiry = () => {
+  const [enquiry, setEnquiry] = useState("");
+  const [contact, setContact] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (enquiry && contact) {
+      const whatsappMessage = queryString.stringify({
+        text: `Enquiry: ${enquiry}\nContact: ${contact}`,
+      });
+      window.location.href = `https://wa.me/<+917899338662>?${whatsappMessage}`;
+    } else {
+      alert("Please fill in both fields.");
+    }
+  };
+
   return (
     <>
       <div className="shadow-2xl rounded-lg ">
-        <h5 className="text-center font-bold m-5">Submit Your Equire Here</h5>
+        <h5 className="text-center font-bold m-5">Submit Your Enquiry Here</h5>
         <div className="text-center m-2">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <textarea
-              name=""
-              id=""
-              className="border border-gray-200 rounded-md h-full w-full text-sm p-2 "
+              name="enquiry"
+              id="enquiry"
+              className="border border-gray-200 rounded-md h-full w-full text-sm p-2"
               placeholder="Please type your enquiry in detail...."
+              value={enquiry}
+              onChange={(e) => setEnquiry(e.target.value)}
             ></textarea>
             <input
               type="text"
               className="text-sm p-3 w-full border border-gray-200 rounded-md"
               placeholder="Your Mobile No. or email Address"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
             />
-            <button className="bg-[#EC1F52] mt-5 p-2 rounded text-white w-full font-semibold text-sm">
+            <button
+              type="submit"
+              className="bg-[#EC1F52] mt-5 p-2 rounded text-white w-full font-semibold text-sm"
+            >
               SUBMIT QUERY
             </button>
           </form>
--- utils/latest-news/side-bar/data.js
+++ utils/latest-news/side-bar/data.js
@@ -1,34 +1,41 @@
 export const sidebar = [
-    {
-        id: 1,
-        img: "https://www.transindo-training.com/wp-content/uploads/2022/03/TRAINING-ONLINE-RELIABILITY-MANAGEMENT-FOR-POWER-PLANTS.jpg",
-        title: "Maximize Power Plant Reliability with Atlas Copco Compressors",
-        date: "April 03 2024",
-    },
-    {
-        id: 2,
-        img: "https://tse4.mm.bing.net/th?id=OIP.gIbCSS7PZeNWMiTJIV8NFAHaE5&pid=Api&P=0&h=220",
-        title: "Certified Clean Air Compressors for Pharmaceuticals",
-        date: "April 03 2024"
-    },
-    {
-        id: 3,
-        img: "https://www.maticline.com/Uploads/202011/5fb0b497d5801.jpg",
-        title: "Oil-Free Compressors for PET Bottle Blowing: Clean Air, Total Solutions",
-        date: "April 03 2024"
-    },
-    {
-        id: 4,
-        img: "https://tse4.mm.bing.net/th?id=OIP.rVL45pXgcHmi5o6_JcNjcQHaFO&pid=Api&P=0&h=220",
-        title: "Your Trusted OEM Partner for Compressed Air Solutions",
-        date: "April 03 2024"
-    },
-    {
-        id: 5,
-        img: "https://www.quincycompressor.com/wp-content/uploads/2021/02/iStock-483555304.png",
-        title: "Reliable Compressed Air Solutions for the Oil and Gas Industry",
-        date: "Mar 27, 2024"
-    },
-]
+  {
+    id: 1,
+    img: "/images/industry/TRAINING-ONLINE-RELIABILITY-MANAGEMENT-FOR-POWER-PLANTS.jpg",
+    title: "Maximize Power Plant Reliability with Atlas Copco Compressors",
+    date: "April 03 2024",
+  },
+  {
+    id: 2,
+    img: "public/images/industry/social_img_989_725a67d1bc0fcfc985649042afa7f0ec.png",
+    title: "Certified Clean Air Compressors for Pharmaceuticals",
+    date: "April 03 2024",
+  },
+  {
+    id: 3,
+    img: "public/images/industry/5575-16219865.jpg",
+    title:
+      "Oil-Free Compressors for PET Bottle Blowing: Clean Air, Total Solutions",
+    date: "April 03 2024",
+  },
+  {
+    id: 4,
+    img: "public/images/industry/th.jpeg",
+    title: "Your Trusted OEM Partner for Compressed Air Solutions",
+    date: "April 03 2024",
+  },
+  {
+    id: 5,
+    img: "public/images/industry/iStock-483555304.jpg",
+    title: "Reliable Compressed Air Solutions for the Oil and Gas Industry",
+    date: "Mar 27, 2024",
+  },
+];
 
-export const socialIcons = ["http://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png", "https://pngimg.com/uploads/linkedIn/linkedIn_PNG8.png", "https://cdn1.iconfinder.com/data/icons/social-links/410/twitter-256.png", "https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-5-1.png", "https://pngimg.com/uploads/pinterest/pinterest_PNG75.png"]
+export const socialIcons = [
+  "http://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png",
+  "https://pngimg.com/uploads/linkedIn/linkedIn_PNG8.png",
+  "https://cdn1.iconfinder.com/data/icons/social-links/410/twitter-256.png",
+  "https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-5-1.png",
+  "https://pngimg.com/uploads/pinterest/pinterest_PNG75.png",
+];
